5d6fc8dd42e0a872e11b7a5011ba2e93db4c8758
matheus silva <103462698+teu0226@users.noreply.github.com>
2022-11-12 17:42:07 -0300

    finalizando aula condicionais-switch

--- 7_CONDICIONAIS-Switch/scripts.js
+++ 7_CONDICIONAIS-Switch/scripts.js
@@ -1,8 +1,37 @@
-var nota1 = 5.0;
-var nota2 = 5.8;
+var nota1 = 4.0;
+var nota2 = 6.8;
 
 var media = (nota1 + nota2) / 2;
 
-if (media > 7) {
-  console.log("você passou com média" + media);
+var conceito = "";
+
+if (media >= 8) {
+  conceito = "ótimo";
+} else if (media > 6.5) {
+  conceito = "bom";
+} else {
+  conceito = "regular";
+}
+
+conceito = "Mais ou menos";
+
+console.log(media);
+console.log(conceito);
+
+switch (conceito) {
+  case "ótimo":
+    console.log("parabéns, vc e um ótimo aluno");
+    break;
+
+  case "bom":
+    console.log("você estar quase perfeito");
+    break;
+
+  case "regular":
+    console.groupCollapsed("estude mais um pouco!");
+    break;
+
+  default:
+    console.log("Houve algum erro");
+    break;
 }
